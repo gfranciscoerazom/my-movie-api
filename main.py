97bd61f99884061b4fff1c8d0457e125f961f5e8
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-07-30 21:58:49 -0500

    Add /movies endpoint

--- main.py
+++ main.py
@@ -5,16 +5,31 @@ app = FastAPI()
 app.title = "My First API with FastAPI"
 app.version = "v0.0.1"
 
-@app.get("/", tags=["Home", "Root"])
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    } 
+]
+
+@app.get("/", tags=["Home"])
 def home():
     return HTMLResponse("""
-    <html>
-        <head>
-            <title>My First API with FastAPI</title>
-        </head>
-        <body>
-            <h1>Welcome to my API</h1>
-            <p>Try to go to <a href="/docs">/docs</a></p>
-        </body>
-    </html>
-""")
+        <html>
+            <head>
+                <title>My First API with FastAPI</title>
+            </head>
+            <body>
+                <h1>Welcome to my API</h1>
+                <p>Try to go to <a href="/docs">/docs</a></p>
+            </body>
+        </html>
+    """)
+
+@app.get("/movies", tags=["Movies"])
+def get_movies():
+    return movies
